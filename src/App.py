fc5cc4b3874fcbe1f7617efe5057bfb4cff03ef0
Jorn Van Dijck <jorn.van.dijck@gmail.com>
2023-11-09 18:52:55 +0200

    progress in jupyter notebook

--- src/App.py
+++ src/App.py
@@ -1,2 +1,45 @@
+import imageio.v3 as iio
+import numpy as np
+import matplotlib.pyplot as plt
+import open3d as o3d
+
+# Depth camera parameters:
+FX_DEPTH = 5.8262448167737955e+02
+FY_DEPTH = 5.8269103270988637e+02
+CX_DEPTH = 3.1304475870804731e+02
+CY_DEPTH = 2.3844389626620386e+02
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    depth_image = iio.imread("." + "/images/png/depth/2023-11-08.20.31.23_depth_image.png")
+    
+    print(f"Image resolution: {depth_image.shape}")
+    print(f"Data type: {depth_image.dtype}")
+    print(f"Min value: {np.min(depth_image)}")
+    print(f"Max value: {np.max(depth_image)}")
+    
+    depth_grayscale = np.array(256 * depth_image / 0x0fff,
+                            dtype=np.uint8)
+    iio.imwrite('output/grayscale.png', depth_grayscale)
+    
+    # Display depth and grayscale image:
+    fig, axs = plt.subplots(1, 2)
+    axs[0].imshow(depth_image, cmap="gray")
+    axs[0].set_title('Depth image')
+    axs[1].imshow(depth_grayscale, cmap="gray")
+    axs[1].set_title('Depth grayscale image')
+    plt.show()
+    
+    # compute point cloud:
+    pcd = []
+    height, width = depth_image.shape
+    for i in range(height):
+        for j in range(width):
+            z = depth_image[i][j]
+            x = (j - CX_DEPTH) * z / FX_DEPTH
+            y = (i - CY_DEPTH) * z / FY_DEPTH
+            pcd.append([x, y, z])
+    
+    pcd_o3d = o3d.geometry.PointCloud()  # create point cloud object
+    pcd_o3d.points = o3d.utility.Vector3dVector(pcd)  # set pcd_np as the point cloud points
+    # Visualize:
+    o3d.visualization.draw_geometries([pcd_o3d])
