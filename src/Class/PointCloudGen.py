d80a9718d6e551660bd1d75989447c348b7d30ae
Jorn Van Dijck <jorn.van.dijck@gmail.com>
2023-11-14 18:01:33 +0200

    PointCloudGeneration class ready for testing

--- src/Class/PointCloudGen.py
+++ src/Class/PointCloudGen.py
@@ -1,7 +1,11 @@
 import numpy as np
 import imageio.v3 as iio
+import open3d as o3d
 
 class PointCloudGen:
+    
+    # Point cloud object
+    __pcd_o3d = None
     
     # Depth camera parameters
     __FX_DEPTH = None
@@ -29,25 +33,176 @@ class PointCloudGen:
     __rgb_image = None
     
     
+    # Constructor
     def __init__(self):
         pass
-        
+    
+    """
+    Set depth camera parameters
+    ATTRIBUTES
+    ----------
+    fx : float
+        Focal length in x
+    fy : float
+        Focal length in y
+    ppx : float
+        Principal point in x
+    ppy : float
+        Principal point in y
+    """
     def set_depth_cam_settings(self, fx, fy, ppx, ppy):
         self.__FX_DEPTH = fx
         self.__FY_DEPTH = fy
         self.__CX_DEPTH = ppx
         self.__CY_DEPTH = ppy
     
+    """
+    Set RGB camera parameters
+    ATTRIBUTES
+    ----------
+    fx : float
+        Focal length in x
+    fy : float
+        Focal length in y
+    ppx : float
+        Principal point in x
+    ppy : float
+        Principal point in y
+    """
     def set_rgb_cam_settings(self, fx, fy, ppx, ppy):
         self.__FX_RGB = fx
         self.__FY_RGB = fy
         self.__CX_RGB = ppx
         self.__CY_RGB = ppy
         
+    """
+    Set depth image path
+    ATTRIBUTES
+    ----------
+    path : string
+        Path to the depth image
+    """
     def set_depth_image_path(self, path):
         self.__depth_image_path = path
         self.__depth_image = iio.imread(path)
         
+    """
+    Set RGB image path
+    ATTRIBUTES
+    ----------
+    path : string
+        Path to the RGB image
+    """
     def set_rgb_image_path(self, path):
         self.__rgb_image_path = path
         self.__rgb_image = iio.imread(path)
+    
+    """
+    Get depth image
+    TODO: Add check for None values
+    RETURNS
+    -------
+    image : numpy array
+        Depth image
+    """
+    def get_depth_image(self):
+        return self.__depth_image
+    
+    """
+    Get RGB image
+    TODO: Add check for None values
+    RETURNS
+    -------
+    image : numpy array
+        RGB image
+    """
+    def get_rgb_image(self):
+        return self.__rgb_image
+
+    """
+    Private method: Calculate points for the pointcloud
+    TODO: Add check for None values
+    """
+    def __calculate_points(self):
+        height, width, index = self.__depth_image.shape
+
+        self.__pcd = []
+        self.__colors = []
+        
+        for i in range(height):
+            for j in range(width):
+                """
+                    Convert the pixel from depth coordinate system
+                    to depth sensor 3D coordinate system
+                """
+                z = self.__depth_image[i][j]
+                x = (j - self.__CX_DEPTH) * z / self.__FX_DEPTH
+                y = (i - self.__CY_DEPTH) * z / self.__FY_DEPTH
+
+                """
+                    Convert the point from depth sensor 3D coordinate system
+                    to rgb camera coordinate system:
+                """
+                [x_RGB, y_RGB, z_RGB] = np.linalg.inv(self.__R).dot([x, y, z]) - np.linalg.inv(self.__R).dot(self.__T)
+
+                """
+                    Convert from rgb camera coordinates system
+                    to rgb image coordinates system:
+                """
+                j_rgb = int((x_RGB[0] * self.__FX_RGB) / z_RGB[0] + self.__CX_RGB + width / 2)
+                i_rgb = int((y_RGB[0] * self.__FY_RGB) / z_RGB[0] + self.__CY_RGB)
+
+                # Add point to point cloud:
+                self.__pcd.append([x[0], y[0], z[0]])
+
+                # Add the color of the pixel if it exists:
+                if 0 <= j_rgb < width and 0 <= i_rgb < height:
+                    self.__colors.append(self.__rgb_image[i_rgb][j_rgb] / 255)
+                else:
+                    self.__colors.append([0., 0., 0.])
+                    
+    """
+    Generate point cloud
+    """
+    def generate_point_cloud(self):
+        
+        self.__calculate_points()
+        
+        self.__pcd_o3d = o3d.geometry.PointCloud()  # create point cloud object
+        self.__pcd_o3d.points = o3d.utility.Vector3dVector(self.__pcd)  # set pcd_np as the point cloud points
+        self.__pcd_o3d.colors = o3d.utility.Vector3dVector(self.__colors)
+    
+    """
+    Get point cloud
+    TODO: Add check for None values
+    RETURNS
+    -------
+    pcd : open3d.geometry.PointCloud
+        Point cloud
+    """
+    def get_point_cloud(self):
+        return self.__pcd_o3d
+    
+    """
+    Export point cloud to .ply file
+    TODO: Add check for None values
+    TODO: Check if the folder where the file is going to be saved exists
+    ATTRIBUTES
+    ----------
+    export_path : string
+        Path to the .ply file
+    
+    RETURNS
+    -------
+    export_path : string
+        Path to the .ply file
+    """
+    def export_point_cloud(self, export_path):
+        
+        if not export_path.endswith('.ply'):
+            export_path += '.ply'
+            
+        o3d.io.write_point_cloud(export_path, self.__pcd_o3d)
+        
+        return export_path
+    
